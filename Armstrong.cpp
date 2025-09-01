f18d34d15c6ce304c0f925d6a7a55b5a04765e7e
somya <somyasharma0488@gmail.com>
2025-09-01 22:42:41 +0530

    main

--- Armstrong.cpp
+++ Armstrong.cpp
@@ -3,9 +3,26 @@ using namespace std;
 
 int main(){
     int n;
+    cout<<"Enter a number: ";
     cin>>n;
 
+    int sum=0;
+    int temp=n;
 
-    }
+//Armstrong Number
+while (temp>0) {
+    int a=temp%10;
+    sum+=a*a*a;
+    temp=temp/10;
 
-    
+}
+
+
+if (sum==n) {
+    cout<<n<<" is an Armstrong number.";
+} else {
+    cout<<n<<" is not an Armstrong number.";
+}
+
+    return 0;
+}   
