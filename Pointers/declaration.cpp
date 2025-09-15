7f5dd45efb1b3e8b7c5399a8ce17e998d6e1d0bf
somya <somyasharma0488@gmail.com>
2025-09-15 07:34:09 +0530

    main

--- Pointers/declaration.cpp
+++ Pointers/declaration.cpp
@@ -2,3 +2,17 @@
 //&-address of operator
 //*-value of operator
 
+#include <iostream>
+using namespace std;
+
+int main(){
+    int num=10;
+    cout<<num<<endl;
+    int *ptr=&num;
+
+    cout<<"Address is: " << ptr << endl;
+    cout<<"Value is: "<< *ptr << endl;
+
+
+    return 0;
+}
